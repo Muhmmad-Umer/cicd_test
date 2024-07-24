93023965de5169d7a4d2b03e0c660b765b8ebb38
Muhammad Umer <umertariq7a@gmail.com>
2024-07-24 15:32:50 +0500

    Added K8 deployments and health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { get } from 'http';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get()
+  health() {
+    return true;
+  }
 
   @Get('version')
   async getVersion(): Promise<string> {
